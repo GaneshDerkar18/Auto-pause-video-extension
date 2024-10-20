253e861a7ad00fac2736ebfae2712b82e77d0db1
Aman8593 <amanwadgaonkar8593@gmail.com>
2024-10-21 00:53:01 +0530

    Implemented autoplay

--- background.js
+++ background.js
@@ -1,10 +1,14 @@
+// Pause other videos when a tab is activated
 browser.tabs.onActivated.addListener(activeInfo => {
-    browser.tabs.query({}, tabs => {
-      tabs.forEach(tab => {
-        if (tab.id !== activeInfo.tabId) {
-          browser.tabs.sendMessage(tab.id, { action: "pauseVideo" });
-        }
-      });
+  browser.tabs.query({}, tabs => {
+    tabs.forEach(tab => {
+      if (tab.id === activeInfo.tabId) {
+        // Send message to play video on the active tab
+        browser.tabs.sendMessage(tab.id, { action: "playVideo" });
+      } else {
+        // Send message to pause videos on all other tabs
+        browser.tabs.sendMessage(tab.id, { action: "pauseVideo" });
+      }
     });
   });
-  
+});
